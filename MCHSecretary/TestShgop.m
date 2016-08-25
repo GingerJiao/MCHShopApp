5de72ab1456791c3560d0fc8bef2064375ca7c05
gengjingjiao <zhujinzhujin@zhujindeiMac.local>
2016-08-25 18:01:54 +0800

    test
    test

---
+++ MCHSecretary/TestShgop.m
@@ -0,0 +1,13 @@
+//
+//  TestShgop.m
+//  MCHSecretary
+//
+//  Created by zhujin zhujin on 16/8/25.
+//  Copyright © 2016年 朱进. All rights reserved.
+//
+
+#import "TestShgop.h"
+
+@implementation TestShgop
+
+@end
