0016f4b5a80cdb5e6693f2e77dab39349bd51efa
qingbaomeng <439020255@qq.com>
2016-08-25 17:53:26 +0800

    add
    add

---
+++ MCHSecretary/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  MCHSecretary
+//
+//  Created by zhujin zhujin on 16/8/25.
+//  Copyright © 2016年 朱进. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ MCHSecretary/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  MCHSecretary
+//
+//  Created by zhujin zhujin on 16/8/25.
+//  Copyright © 2016年 朱进. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
